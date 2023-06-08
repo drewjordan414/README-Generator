af9a5b3e5292edfe9ab4101a730b66db1c4bdb0b
Drew Jordan <ajordan12@luc.edu>
2023-06-07 19:10:26 -0500

    changed layout

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const fs = require('fs');
 
 
 // array of questions for user
-inquirer
-    .prompt([
+const content = [
         {
             type: 'input',
             message: 'What is your name?',
@@ -13,28 +12,75 @@ inquirer
         },
         {
             type: 'input',
-            message: '',
+            message: 'What is your email address?',
             name: '',
         },
         {
             type: 'input',
-            message: '',
-            name: '',
+            message: 'What is your Github username?',
+            name: 'username',
         },
         {
-            type: 'input',
-            message: '',
-            name: '',
+            type: 'checkbox',
+            choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+            message: 'What license would you like to use?',
+            name: 'license',
         },
-        {
-            type: 'input',
-            message: '',
-            name: '',
-        }
-            .then((response) => {
-                console.log(response);
-                fs.writeFile('README.md', htmlPageContent, (err) =>
-                    err ? console.log(err) : console.log('Successfully created README.md!')
-                );
-            })
-    ])
+        // {
+        //     type: 'input',
+        //     message: '',
+        //     name: '',
+        // }
+           
+    ];
+    // .then((response) => {
+    //     console.log(response);
+    //     fs.writeFile('README.md', htmlPageContent, (err) =>
+    //         err ? console.log(err) : console.log('Successfully created README.md!')
+    //     );
+    // })
+    // .catch((err) => console.error(err));
+
+// function to generate markdown for README
+function generateMarkdown(response) {
+    return `
+    # ${answers.title}
+
+    ## Description
+    
+    ${answers.description}
+    
+    ## Table of Contents
+    
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    
+    ## Installation
+    
+    ${answers.installation}
+    
+    ## Usage
+    
+    ${answers.usage}
+    
+    ## License
+    
+    This project is covered under the ${answers.license} License.
+    
+    ## Contributing
+    
+    ${answers.contribution}
+    
+    ## Tests
+    
+    ${answers.tests}
+    
+    ## Questions
+    
+    For any questions, please reach out to [${answers.username}](https://github.com/${answers.username}) or [Email me](mailto:${answers.email})
+    `;
+}
